0c9d7b0e1fdf39733677f4ce42a4a13a43ea4985
EnderCreators <EnderCreators@gmail.com>
2013-07-18 23:44:22 -0400

    Refresh system v1

--- src/main/java/com/endercreators/perms/EnderPerms.java
+++ src/main/java/com/endercreators/perms/EnderPerms.java
@@ -1,6 +1,106 @@
 package com.endercreators.perms;
 
 
-public class EnderPerms {
-    // :)
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class EnderPerms extends JavaPlugin {
+
+    private MySQLManager mysqlmanager;
+
+    public HashMap<String, List<String>> cachegroup = new HashMap<String, List<String>>();
+
+    public static Permission perms = null;
+    public static Chat chat = null;
+
+    @Override
+    public void onEnable() {
+        setupPermissions();
+        setupChat();
+
+        mysqlmanager = new MySQLManager(this);
+
+        if (getConfig().getLong("refresh") < 60)
+            getLogger().info("Warning: Refresh time is under 60 secs.");
+
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
+                refresh();
+            }
+        }, 60, getConfig().getLong("refresh") * 20);
+
+    }
+
+    public void go(Runnable runnable) {
+        Bukkit.getScheduler().runTaskLater(this, runnable, 0);
+    }
+
+    public MySQLManager getMySQLManager() {
+        return mysqlmanager;
+    }
+
+    public EnderPerms getPlugin() {
+        return this;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public void refresh() {
+        go(new Runnable() {
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    removeFromAllGroups(player);
+                    addToGroup(player, getMySQLManager().getGroup(player.getName()));
+                    addToGroup(player, getConfig().getString("default"));
+                }
+            }
+        });
+
+    }
+
+    public void addToGroup(Player player, String group) {
+        perms.playerAddGroup(player, group);
+        List<String> groups = cachegroup.get(player.getName());
+        groups.add(group);
+        cachegroup.remove(player.getName());
+        cachegroup.put(player.getName(), groups);
+    }
+
+    public void removeFromGroup(Player player, String group) {
+        perms.playerRemoveGroup(player, group);
+        List<String> groups = cachegroup.get(player.getName());
+        groups.remove(group);
+        cachegroup.remove(player.getName());
+        cachegroup.put(player.getName(), groups);
+    }
+
+    public void removeFromAllGroups(Player player) {
+        List<String> groups = cachegroup.get(player.getName());
+        for (String group : groups) {
+            removeFromGroup(player, group);
+        }
+        cachegroup.remove(player.getName());
+    }
 }
---
+++ src/main/java/com/endercreators/perms/MySQLManager.java
@@ -0,0 +1,105 @@
+package com.endercreators.perms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQLManager {
+
+    public static String host;
+    public static String database;
+    public static String user;
+    public static String pass;
+    private EnderPerms plugin;
+
+    public MySQLManager(EnderPerms plugin) {
+        host = plugin.getConfig().getString("mysql.host");
+        database = plugin.getConfig().getString("mysql.database");
+        user = plugin.getConfig().getString("mysql.user");
+        pass = plugin.getConfig().getString("mysql.pass");
+        this.plugin = plugin;
+        plugin.go(new Runnable() {
+            public void run() {
+                fix();
+            }
+        });
+
+    }
+
+
+    // Connector
+    public static Connection getConn() {
+        Connection conn = null;
+        try {
+            String uri = "jdbc:mysql://" + host + "/" + database;
+            conn = DriverManager.getConnection(uri, user, pass);
+            return conn;
+        } catch (Exception ex) {
+        }
+
+        try {
+            fix();
+            String uri = "jdbc:mysql://" + host + "/" + database;
+            conn = DriverManager.getConnection(uri, user, pass);
+            return conn;
+        } catch (Exception ex2) {
+        }
+
+        System.out.println("[MySQL] Error caused in MySQL. Cannot connect to database.. Make sure that database '" + database + "' exist.");
+
+        return conn;
+    }
+
+    public void setGroup(String player, String group) {
+
+        try {
+            Connection conn = getConn();
+            Statement st = conn.createStatement();
+            st.execute("SELECT * FROM `" + database + "`.`groups` WHERE player='" + player + "'");
+            ResultSet rs = st.getResultSet();
+            if (!rs.next()) {
+                st.execute("INSERT INTO `" + database + "`.`groups` VALUES('" + player
+                        + "'" + group + "')");
+            } else {
+                st.execute("UPDATE `" + database + "`.`groups` SET group='" + group + "' WHERE player='" + player + "'");
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("[MySQL] Error caused in setting group. Player: " + player + " Error: " + e);
+        }
+
+    }
+
+    public String getGroup(String player) {
+
+        try {
+            Connection conn = getConn();
+            Statement st = conn.createStatement();
+            st.execute("SELECT * FROM `" + database + "`.`groups` WHERE player='" + player + "'");
+            ResultSet rs = st.getResultSet();
+            if (rs.next()) {
+                return rs.getString("group");
+            }
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println("[MySQL] Error caused in getting group. Player: " + player + " Error: " + e);
+        }
+        return null;
+    }
+
+
+    private static void fix() {
+        try {
+            String uri = "jdbc:mysql://" + host + "/";
+            Connection conn = DriverManager.getConnection(uri, user, pass);
+            conn.createStatement().execute("CREATE TABLE IF NOT EXISTS `"
+                    + database
+                    + "`.groups` ( `player` VARCHAR(32) NOT NULL, `group` VARCHAR(32))");
+        } catch (Exception e) {
+            System.out.println("[MySQL] Error caused while setting up table. Make sure that the database exist. Error: " + e);
+        }
+    }
+
+}
